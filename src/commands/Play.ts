339d27119ff8e17dbc4994ce12e8af95a5cc7107
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-12-27 21:59:51 -0300

    Move play logic into play command class

--- src/commands/Play.ts
+++ src/commands/Play.ts
@@ -1,17 +1,54 @@
 import { VoiceConnection } from "@discordjs/voice";
-import MusicPlayer from "../utils/MusicPlayer";
+import { Message, StageChannel, VoiceChannel } from "discord.js";
 import { Track } from "../utils/Track";
 import { ICommand } from "./ICommand";
+import { connectToChannel } from "../utils/connectToChannel";
+
+import MusicPlayer from "../utils/MusicPlayer";
 
 export class PlayCommand implements ICommand {
-  constructor(private track: Track, private connection: VoiceConnection) {}
+  channel: VoiceChannel | StageChannel | null | undefined;
 
-  async execute(): Promise<void> {
-    this.playSong();
+  constructor(private message: Message, private args: string) {}
+
+  async execute() {
+    const channel = this.message.member?.voice.channel;
+
+    if (!channel) {
+      // Disallow playing a song if user is not currently on a voice channel
+      this.message.channel.send({
+        content: `${this.message.author} please join a voice channel before using !play`,
+      });
+      return;
+    }
+
+    const connection = await connectToChannel(channel);
+
+    this.playSong(connection);
   }
 
-  private async playSong() {
-    MusicPlayer.setVoiceConnection(this.connection);
-    MusicPlayer.enqueue(this.track);
+  private async playSong(connection: VoiceConnection) {
+    const track = await Track.from(this.args, {
+      onStart: async () => {
+        await this.message.reply(`Started playing ${track.title}`);
+      },
+      onEnqueue: async (queue: Track[]) => {
+        await this.message.reply(
+          `Queuing ${track.title} at position ${queue.length - 1}`
+        );
+      },
+
+      onFinish: async () => {
+        console.log("Finished playing song");
+      },
+
+      onError: async (error) => {
+        await this.message.reply(
+          `Something went wrong trying to play ${track.title}`
+        );
+      },
+    });
+    MusicPlayer.setVoiceConnection(connection);
+    MusicPlayer.enqueue(track);
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,6 @@
 import "dotenv/config";
-import {
-  Client,
-  Intents,
-  Message,
-  Guild,
-  User,
-  VoiceChannel,
-  StageChannel,
-} from "discord.js";
-import {
-  joinVoiceChannel,
-  entersState,
-  VoiceConnectionStatus,
-  createAudioResource,
-  StreamType,
-  AudioPlayerStatus,
-  createAudioPlayer,
-} from "@discordjs/voice";
+import { Client, Intents, Message } from "discord.js";
+import { PlayCommand } from "./commands/Play";
 
 const client = new Client({
   intents: [
@@ -26,10 +10,7 @@ const client = new Client({
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
   ],
 });
-import { createDiscordJSAdapter } from "./adapter";
-import { PlayCommand } from "./commands/Play";
-import { Track } from "./utils/Track";
-import { connectToChannel } from "./utils/connectToChannel";
+
 client.once("ready", () => {
   console.log("Ready!");
 });
@@ -38,44 +19,11 @@ client.on("messageCreate", async (message: Message) => {
   const prefix = "!";
   if (message.content.startsWith(prefix)) {
     const [command, args] = message.content.split(" ");
+
     switch (command) {
       case "!play":
-        const channel = message.member?.voice.channel;
-        if (channel) {
-          try {
-            const connection = await connectToChannel(channel);
-            const track = await Track.from(args, {
-              async onStart() {
-                await message.reply(`Started playing ${track.title}`);
-              },
-
-              async onEnqueue(queue) {
-                await message.reply(
-                  `Queuing ${track.title} at position ${queue.length - 1}`
-                );
-              },
-              onError(err) {
-                console.log(err);
-              },
-
-              onFinish() {
-                console.log("Finished playing track");
-              },
-            });
-
-            const playCmd = new PlayCommand(track, connection);
-
-            await playCmd.execute();
-          } catch (error) {
-            console.error(error);
-          }
-        } else {
-          message.channel.send({
-            content: `${message.author} please join a voice channel before using !play`,
-          });
-          return;
-        }
-
+        const playCmd = new PlayCommand(message, args);
+        await playCmd.execute();
         break;
     }
   }
